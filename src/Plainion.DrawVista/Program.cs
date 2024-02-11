3a9c92c9560c233d771b010133eaf052b8520d6f
plainionist <stanley751@gmx.net>
2024-02-11 18:16:51 +0100

    WebApi created

--- src/Plainion.DrawVista/Program.cs
+++ src/Plainion.DrawVista/Program.cs
@@ -1,24 +1,47 @@
-﻿using Plainion.DrawVista.Adapters;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Plainion.DrawVista.Adapters;
 using Plainion.DrawVista.IO;
 using Plainion.DrawVista.UseCases;
 
-namespace Plainion.DrawVista;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.Environment.ContentRootPath = Path.GetDirectoryName(typeof(SvgProcessor).Assembly.Location);
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var drawIOFile = args[0];
-
-        Console.WriteLine($"Analyzing file: {drawIOFile}");
-
-        var outputFolder = Path.Combine("src", "browser", "src", "assets");
-        var drawIoWorkbook = new DrawIOWorkbook(drawIOFile, outputFolder);
-
-        var svgProcessor = new SvgProcessor(
-            new SvgCaptionParser(),
-            new SvgHyperlinkFormatter());
-
-        svgProcessor.Process(drawIoWorkbook);
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+var outputFolder = Path.Combine(app.Environment.ContentRootPath, "store");
+if (!Directory.Exists(outputFolder))
+{
+    Directory.CreateDirectory(outputFolder);
+}
+
+var drawIOFile = Path.Combine(app.Environment.ContentRootPath, "samples", "sample.drawio");
+
+app.MapGet("/test", () =>
+{
+    Console.WriteLine($"Analyzing file: {drawIOFile}");
+
+    var drawIoWorkbook = new DrawIOWorkbook(drawIOFile, outputFolder);
+
+    var svgProcessor = new SvgProcessor(
+        new SvgCaptionParser(),
+        new SvgHyperlinkFormatter());
+
+    svgProcessor.Process(drawIoWorkbook);
+    
+    return "OK";
+});
+
+app.Run();
