15fd80a54da6923c53e8b01b3986713261aad3c7
plainionist <stanley751@gmx.net>
2024-02-26 08:35:24 +0100

    avoid concurrency issues in DocumentStore

--- src/Plainion.DrawVista/IO/DocumentStore.cs
+++ src/Plainion.DrawVista/IO/DocumentStore.cs
@@ -4,25 +4,38 @@ namespace Plainion.DrawVista.IO;
 
 public class DocumentStore(string RootFolder) : IDocumentStore
 {
-    public IReadOnlyCollection<RawDocument> GetAllFiles() =>
-        Directory.GetFiles(RootFolder, "*.svg")
-            .Select(f => new RawDocument(Path.GetFileNameWithoutExtension(f), File.ReadAllText(f)))
-            .ToList();
+    private readonly object myLock = new object();
+
+    public IReadOnlyCollection<RawDocument> GetAllFiles()
+    {
+        lock (myLock)
+        {
+            return Directory.GetFiles(RootFolder, "*.svg")
+                .Select(f => new RawDocument(Path.GetFileNameWithoutExtension(f), File.ReadAllText(f)))
+                .ToList();
+        }
+    }
 
     public string GetFileName(string pageName) =>
         Path.Combine(RootFolder, pageName + ".svg");
 
     public void Save(SvgDocument document)
     {
-        var svgFile = Path.Combine(RootFolder, document.Name + ".svg");
-        File.WriteAllText(svgFile, document.Content.ToString());
+        lock (myLock)
+        {
+            var svgFile = Path.Combine(RootFolder, document.Name + ".svg");
+            File.WriteAllText(svgFile, document.Content.ToString());
+        }
     }
 
     public void Clear()
     {
-        foreach (var file in Directory.GetFiles(RootFolder))
+        lock (myLock)
         {
-            File.Delete(file);
+            foreach (var file in Directory.GetFiles(RootFolder))
+            {
+                File.Delete(file);
+            }
         }
     }
 }
