af743f10298bd5d90cbdd28e8cf8a1196b047cca
Friedemann Braune <friedemann.braune@gmail.comn>
2024-07-26 23:42:47 +0200

    minor style changes

--- src/Plainion.DrawVista/WebUI_quasar/src/layouts/MainLayout.vue
+++ src/Plainion.DrawVista/WebUI_quasar/src/layouts/MainLayout.vue
@@ -1,16 +1,7 @@
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Plainion.DrawVista
-        </q-toolbar-title>
-      </q-toolbar>
-
-      <q-tabs align="left">
+      <q-tabs align="center">
         <q-route-tab v-for="route in routes" :key="route.name" :to="route.path" :label="route.name?.toString() ?? 'undefined'" />
       </q-tabs>
     </q-header>
